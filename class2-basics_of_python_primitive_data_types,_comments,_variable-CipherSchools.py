afeccc57125b689edda587115551004eab841e47
TANUSH ARORA <tanusharora@192.168.1.8>
2022-11-28 23:22:02 +0530

    fourth commit

---
+++ class2-basics_of_python_primitive_data_types,_comments,_variable-CipherSchools.py
@@ -0,0 +1,35 @@
+### Commands
+- to execute code shift + enter
+- to create a block below current blocks b
+- to create a block above current blocks:a
+- to delete current block:dd
+
+print("Hello")
+Hello
+
+Every line executed in python is either
+1. Statement
+2. Expression
+
+
+# this is a comment
+
+
+a=1
+print(a)
+
+a=200
+print(a)
+
+type(a)
+
+a=225
+print(id(a))
+
+print(hex(id(a)))
+
+b=278
+print(id(b))
+
+c=278
+print(id(c))
